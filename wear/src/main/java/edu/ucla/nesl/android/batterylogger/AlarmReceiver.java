d7e91f66817483886aa7722b621b46a49a3ddf56
Chenguang Shen <pscggeforce@gmail.com>
2015-04-21 19:16:50 -0700

    first commit

---
+++ wear/src/main/java/edu/ucla/nesl/android/batterylogger/AlarmReceiver.java
@@ -0,0 +1,55 @@
+package edu.ucla.nesl.android.batterylogger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.PowerManager;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by cgshen on 4/21/15.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+    private static final String TAG = "Wear/BroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "SensorCollector");
+        wl.acquire();
+        Log.i(TAG, "Alarm Received");
+
+        try {
+            IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = context.registerReceiver(null, ifilter);
+            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            float batteryPct = level / (float)scale;
+
+            BufferedWriter outputBattery = new BufferedWriter(new FileWriter("/storage/sdcard0/sensor_data/battery_normal_.txt", true));
+
+            if (batteryPct > 0.2) {
+                outputBattery.append(String.valueOf(System.currentTimeMillis()) + "," + String.valueOf(batteryPct) + "\n");
+                outputBattery.flush();
+
+                Log.d(TAG, "Battery level = " + batteryPct);
+            }
+            else {
+                outputBattery.flush();
+                outputBattery.close();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+       wl.release();
+    }
+}
---
+++ wear/src/main/java/edu/ucla/nesl/android/batterylogger/MainActivity.java
@@ -0,0 +1,35 @@
+package edu.ucla.nesl.android.batterylogger;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        AlarmManager alarmMgr = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
+                1000 * 60 * 10, alarmIntent);
+    }
+}
