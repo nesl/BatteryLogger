5bdff89361f991deea6a644ef880deeeaea987b5
Chenguang Shen <pscggeforce@gmail.com>
2015-07-23 15:46:18 -0700

    read android battery gauge for current and voltage

---
+++ mobile/src/main/java/edu/ucla/nesl/android/batterylogger/BatteryLogger.java
@@ -0,0 +1,35 @@
+package edu.ucla.nesl.android.batterylogger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by cgshen on 7/22/15.
+ */
+public class BatteryLogger extends BroadcastReceiver{
+    private static final String TAG = "Mobile/BatteryLogger";
+    public static final String START_MEASUREMENT = "START";
+    public static final String STOP_MEASUREMENT = "STOP";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Broadcast received: " + intent.getAction());
+
+        if (intent.getAction().contains(START_MEASUREMENT)) {
+            Intent startServiceIntent = new Intent(context, BatteryLoggerIntentService.class);
+            startServiceIntent.setAction(START_MEASUREMENT);
+            context.startService(startServiceIntent);
+        }
+        else if (intent.getAction().contains(STOP_MEASUREMENT)) {
+            Intent startServiceIntent = new Intent(context, BatteryLoggerIntentService.class);
+            startServiceIntent.setAction(STOP_MEASUREMENT);
+            context.startService(startServiceIntent);
+        }
+        else {
+            Log.i(TAG, "Unknown action");
+        }
+
+    }
+}
---
+++ mobile/src/main/java/edu/ucla/nesl/android/batterylogger/BatteryLoggerIntentService.java
@@ -0,0 +1,89 @@
+package edu.ucla.nesl.android.batterylogger;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by cgshen on 7/22/15.
+ */
+public class BatteryLoggerIntentService extends IntentService {
+    private static final String TAG = "Mobile/BatteryLoggerIntentService";
+    private static final String CURRENT_NOW = "/sys/class/power_supply/battery/current_now";
+    private static final String VOLTAGE_NOW = "/sys/class/power_supply/battery/voltage_now";
+    private String LOG_NAME = "/sdcard/powerlog/powerlog_" + System.currentTimeMillis() + ".csv";
+    private static ScheduledExecutorService mScheduleTaskExecutor;
+    private static ScheduledFuture scheduledFuture;
+
+    @Override
+    protected void onHandleIntent(Intent workIntent) {
+        if (workIntent.getAction().contains(BatteryLogger.START_MEASUREMENT)) {
+            LOG_NAME = "/sdcard/powerlog/powerlog_" + System.currentTimeMillis() + ".csv";
+            Log.i(TAG, "Start battery logging...");
+            scheduledFuture = mScheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+                @Override
+                public void run() {
+                    logPower();
+                }
+            }, 0, 1, TimeUnit.SECONDS);
+        }
+        else if (workIntent.getAction().contains(BatteryLogger.STOP_MEASUREMENT)) {
+            Log.i(TAG, "Stop battery logging...");
+            if (scheduledFuture != null) {
+                scheduledFuture.cancel(false);
+            }
+            else {
+                Log.i(TAG, "scheduled future null!");
+            }
+
+            mScheduleTaskExecutor.shutdown();
+        }
+    }
+
+    private void logPower() {
+        float currentNow = readValue(CURRENT_NOW);
+        float voltageNow = readValue(VOLTAGE_NOW);
+        try {
+            BufferedWriter buf = new BufferedWriter(new FileWriter(LOG_NAME, true));
+            buf.write(System.currentTimeMillis() + "," + voltageNow + "," + currentNow + "\n");
+            buf.flush();
+            buf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private float readValue(String fileName) {
+        float result = 0.0f;
+
+        try {
+            BufferedReader buf = new BufferedReader(new FileReader(fileName));
+            result = Float.parseFloat(buf.readLine().trim());
+            buf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public BatteryLoggerIntentService() {
+        super("BatteryLoggerIntentService");
+        mScheduleTaskExecutor = Executors.newScheduledThreadPool(1);
+    }
+}
